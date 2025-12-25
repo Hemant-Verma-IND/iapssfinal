b243e6781e84bfd759e0a3009b0c81e9a26a1746
Hemant Verma <152959300+Hemant-Verma-IND@users.noreply.github.com>
2025-12-25 15:24:49 +0530

    Fixed User Schema Bug for Google and Github OAuth

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -4,8 +4,24 @@ const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    passwordHash: { type: String, required: true },
-
+    passwordHash: { type: String,     required: function() {
+      // Only require password if the user does NOT have a googleId or githubId
+      return !this.googleId && !this.githubId;
+    } },
+      googleId: {
+    type: String,
+    unique: true,
+    sparse: true // Important: allows multiple users to have null googleId
+  },
+  githubId: {
+    type: String,
+    unique: true,
+    sparse: true
+  },
+  provider: {
+    type: String,
+    default: "email"
+  },
     role: {
       type: String,
       enum: ["user", "admin"],
